c5027e543b2bebbb9d97da7da01b59fe583f6ec4
arm1nt <armin.nikbakht@gmail.com>
2025-01-31 22:26:34 +0100

    Add header with string utility functions

---
+++ server/util/string_util.h
@@ -0,0 +1,38 @@
+#ifndef RESYNC_STRING_UTIL_H
+#define RESYNC_STRING_UTIL_H
+
+#include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <string.h>
+
+typedef struct StringList {
+    char *val;
+    struct StringList *next;
+} StringList;
+
+static char *
+resync_strdup(const char *str) {
+    return (str == NULL) ? NULL : strdup(str);
+}
+
+static bool
+is_blank_string(const char *str)
+{
+    if (str == NULL) {
+        return false;
+    }
+
+    size_t index = 0;
+
+    while (str[index] != '\0') {
+        if (isspace(str[index]) == 0) {
+            return false;
+        }
+        index++;
+    }
+
+    return true;
+}
+
+#endif //RESYNC_STRING_UTIL_H

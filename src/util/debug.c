8b44f60fab21b0a75ae73d9517eca7b3ab37836d
arm1nt <armin.nikbakht@gmail.com>
2025-02-23 21:39:21 +0100

    Add macro to print messages to stderr

--- src/util/debug.c
+++ src/util/debug.c
@@ -2,13 +2,14 @@
 
 #ifdef RESYNC_LOGGING
 void
-log_msg(const char *fmt, ...)
+log_msg(FILE* stream, const char *fmt, ...)
 {
     va_list args;
     va_start(args, fmt);
 
+
     char *msg = format_string_from_list(fmt, args);
-    fprintf(stdout, "%s\n", msg);
+    fprintf(stream, "%s\n", msg);
 
     va_end(args);
     DO_FREE(msg);
--- src/util/debug.h
+++ src/util/debug.h
@@ -9,11 +9,13 @@
 #include "memory.h"
 
 #ifdef RESYNC_LOGGING
-#define LOG(fmt, ...) log_msg(fmt, ##__VA_ARGS__)
+#define LOG(fmt, ...) log_msg(stdout, fmt, ##__VA_ARGS__)
+#define LOG_ERROR(fmt, ...) log_msg(stderr, fmt, ##__VA_ARGS__)
 
-void log_msg(const char *fmt, ...);
+void log_msg(FILE *stream, const char *fmt, ...);
 #else
 #define LOG(fmt, ...) do {} while(0)
+#define LOG_ERROR(fmt, ...) do {} while (0)
 #endif
 
 #endif //RESYNC_DEBUG_H

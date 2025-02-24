6636808382c6608956223934b480f1f029cd9d05
arm1nt <armin.nikbakht@gmail.com>
2025-02-24 15:06:45 +0100

    Add macros to simplify dealing with setting an error message

--- src/util/error.h
+++ src/util/error.h
@@ -9,6 +9,28 @@
 #include <stdarg.h>
 #include <errno.h>
 
+#define CLEAR_ERROR_MSG(x) do {     \
+        if ((x) != NULL) {          \
+            *(x) = NULL;            \
+        }                           \
+    } while (0)
+
+#define SET_ERROR_MSG_RAW(x,y) do {     \
+        if ((x) != NULL) {              \
+            *(x) = (y);                 \
+        }                               \
+    } while (0)
+
+#define SET_ERROR_MSG(x,y) SET_ERROR_MSG_RAW(x, resync_strdup(y))
+
+#define SET_ERROR_MSG_IF_EMPTY_RAW(x,y) do { \
+        if ((x) != NULL && *(x) == NULL) {   \
+            *(x) = (y);                      \
+        }                                    \
+    } while(0)
+
+#define SET_ERROR_MSG_IF_EMPTY(x,y) SET_ERROR_MSG_IF_EMPTY_RAW(x, resync_strdup(y))
+
 void fatal_error(const char *name);
 
 void fatal_custom_error(const char *fmt, ...);

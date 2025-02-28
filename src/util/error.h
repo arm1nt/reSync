f9ce903e13d50b5955ea1643952fc0e78b7d1d50
arm1nt <armin.nikbakht@gmail.com>
2025-02-28 23:05:26 +0100

    Remove unused macros and add marco to define an error message with a cause

--- src/util/error.h
+++ src/util/error.h
@@ -9,12 +9,6 @@
 #include <stdarg.h>
 #include <errno.h>
 
-#define CLEAR_ERROR_MSG(x) do {     \
-        if ((x) != NULL) {          \
-            *(x) = NULL;            \
-        }                           \
-    } while (0)
-
 #define SET_ERROR_MSG_RAW(x,y) do {     \
         if ((x) != NULL) {              \
             *(x) = (y);                 \
@@ -23,13 +17,24 @@
 
 #define SET_ERROR_MSG(x,y) SET_ERROR_MSG_RAW(x, resync_strdup(y))
 
-#define SET_ERROR_MSG_IF_EMPTY_RAW(x,y) do { \
-        if ((x) != NULL && *(x) == NULL) {   \
-            *(x) = (y);                      \
-        }                                    \
-    } while(0)
+#define SET_ERROR_MSG_WITH_CAUSE_RAW(error_msg, msg_ptr, cause_msg) do { \
+        char *msg = msg_ptr;                                             \
+        if ((error_msg) != NULL) {                                   \
+            char *error_cause = ((cause_msg) != NULL) ? *(cause_msg) : NULL; \
+            if (error_cause != NULL) {                               \
+                *(error_msg) = format_string("%s: %s", msg, error_cause);\
+            } else {                                                 \
+                *(error_msg) = resync_strdup(msg);                                                     \
+            }                                                        \
+            DO_FREE(msg);                                            \
+            DO_FREE(error_cause);                                    \
+        }                                                            \
+    } while (0)
 
-#define SET_ERROR_MSG_IF_EMPTY(x,y) SET_ERROR_MSG_IF_EMPTY_RAW(x, resync_strdup(y))
+#define SET_ERROR_MSG_WITH_CAUSE(error_msg, msg, cause_msg) do { \
+        char *temp = resync_strdup(msg);                         \
+        SET_ERROR_MSG_WITH_CAUSE_RAW(error_msg, temp, cause_msg);\
+    } while (0)
 
 void fatal_error(const char *name);
 
